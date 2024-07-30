1aeb2d3fe3265a70fb9dd4455f8990b319ea1ee2
ella-kadriaeva <ella.kadriaieva@gmail.com>
2024-07-31 00:43:48 +0300

    section portfolio adartiv

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,103 +1,31 @@
 import React from 'react';
-import {
-  SectionConteiner,
-  AncorLink,
-  BlockList,
-  Li,
-  Photo,
-  TextBlock,
-  Text,
-  ColoredText,
-  GreyText,
-  TextContent,
-  TextWrapper,
-  Button,
-  PhotoWrapper,
-  PositionBottom,
-  PositionTop240, LiRevers,
-  PositionTop0,
-  PositionRight0, BtnBlockRevers, RelativBox
-} from './Portfolio.styled';
-import ContentBtnBlock from './SvgHoverLine'
+import { SectionConteiner, AncorLink, Text, ColoredText, GreyText, TextContent, TextWrapper, Button, RelativBox } from './Portfolio.styled';
 import SvgStartLine from './SvgStartLine';
-
+import PhotoItems from './PhotoItems';
 
 const Portfolio = () => {
   return (
     <SectionConteiner id="portfolio">
       <AncorLink>Portfolio</AncorLink>
-      <BlockList>
-          <Li>
-            <PhotoWrapper type='1' >
-              <Photo />
-            </PhotoWrapper>
-            <TextBlock>badroom</TextBlock>
-            <PositionTop240>
-              <PhotoWrapper type='2' size='2'>
-              <Photo  />
-            </PhotoWrapper>
-            </PositionTop240>
-            <PositionBottom>
-            <PhotoWrapper type='3' size='3'>
-              <Photo  />
-            </PhotoWrapper>
-            </PositionBottom>
-            <ContentBtnBlock num={0}/>
-          </Li>
-          <LiRevers>
-            <PhotoWrapper type='4'>
-              <Photo />
-            </PhotoWrapper>
-            <TextBlock>children's</TextBlock>
-            <PositionRight0>
-            <PhotoWrapper type='6' size='4'>
-              <Photo />
-            </PhotoWrapper>
-            </PositionRight0>
-            <PositionTop0>
-            <PhotoWrapper type='5' size='2'>
-              <Photo  />
-            </PhotoWrapper>
-            </PositionTop0>
-            <BtnBlockRevers>
-            <ContentBtnBlock num={1}/>
-            </BtnBlockRevers>
-          </LiRevers>
-          <Li>
-            <PhotoWrapper type='7'>
-              <Photo  />
-            </PhotoWrapper>
-            <TextBlock>kitchen</TextBlock>
-            <PositionTop240>
-            <PhotoWrapper size='1'>
-              <Photo  />
-            </PhotoWrapper>
-            </PositionTop240>
-            <PositionBottom>
-            <PhotoWrapper type='8' size='2'>
-              <Photo  />
-            </PhotoWrapper>
-            </PositionBottom>
-            <ContentBtnBlock num={2}/>
-          </Li>
-    </BlockList>
-
-      <TextWrapper>
+      <PhotoItems />
+      {/* <TextWrapper>
         <TextContent>
           <Text>
             Get a<ColoredText> personalized</ColoredText>
           </Text>
           <Text>calculation of</Text>
           <Text>
-            <b>the cost of <GreyText>your</GreyText></b>
+            <b>
+              the cost of <GreyText>your</GreyText>
+            </b>
           </Text>
           <Text>project</Text>
         </TextContent>
       </TextWrapper>
       <Button>+ sample project design and implementation photos in the messenger</Button>
       <RelativBox>
-      <SvgStartLine/>
-      </RelativBox>
+        <SvgStartLine />
+      </RelativBox> */}
     </SectionConteiner>
   );
 };
--- src/components/Portfolio/Portfolio.styled.jsx
+++ src/components/Portfolio/Portfolio.styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import mediaBp from '../../styles/breakpoints';
 import { setBGImage } from '../../helpers/setBGImage';
-import { setWidthImage, setHeightImage } from '../../helpers/setSizeImage';
+import { setSizeImage, setPositionTop, setPositionLeft, setPadding, setPaddingBottom } from '../../helpers/setSizeImage';
 import { motion } from 'framer-motion';
 export const SectionConteiner = styled.section`
   margin: 0 auto;
@@ -9,94 +9,179 @@ export const SectionConteiner = styled.section`
   }
   ${mediaBp('desktop')} {
     padding: 235px 0 6px 0;
-    max-width: 1920px;
   }
 `;
 export const AncorLink = styled.div`
+  color: #b4b4b4;
+  text-align: justify;
+  font-family: Roboto;
+  font-size: 8px;
+  font-weight: 700;
+  line-height: 120%;
+  text-transform: uppercase;
+  padding-right: 40px;
+  text-align: right;
   ${mediaBp('tablet')} {
+    font-size: 12px;
   }
   ${mediaBp('desktop')} {
-    color: #b4b4b4;
-    text-align: justify;
-    font-family: Roboto;
     font-size: 16px;
-    font-weight: 700;
-    line-height: 120%;
-    text-transform: uppercase;
-
-    padding-right: 40px;
-    text-align: right;
   }
 `;
 
-export const BlockList = styled.ul``;
-
 export const Li = styled.li`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 100px;
   ${mediaBp('tablet')} {
+    flex-direction: row;
+    margin-bottom: 100px;
+    display: flex;
   }
   ${mediaBp('desktop')} {
+    max-width: 1920px;
+    flex-direction: row;
     display: flex;
-    position: relative;
     margin-bottom: 200px;
   }
 `;
 export const LiRevers = styled.li`
+  margin-bottom: 100px;
   ${mediaBp('tablet')} {
+    display: flex;
+    flex-direction: row-reverse;
+    margin-bottom: 100px;
   }
   ${mediaBp('desktop')} {
     display: flex;
-    position: relative;
-    margin-bottom: 200px;
     flex-direction: row-reverse;
+    margin-bottom: 200px;
   }
 `;
-
-export const PhotoWrapper = styled.div`
+export const SquareBox = styled.div`
+  overflow: hidden;
+  position: relative;
+  margin-top: ${setPositionTop}%;
+  left: ${setPositionLeft}%;
+  width: ${setSizeImage};
+  background-image: ${setBGImage};
+  background-size: contain;
+  background-repeat: no-repeat;
+  &:before {
+    content: '';
+    display: block;
+    padding-top: ${setPadding}%;
+    padding-bottom: ${setPaddingBottom}%;
+  }
   ${mediaBp('tablet')} {
+    width: ${setSizeImage};
+    margin-top: ${setPositionTop}%;
+    left: ${setPositionLeft}%;
   }
   ${mediaBp('desktop')} {
-    width: ${setWidthImage};
-    height: ${setHeightImage};
-    background-image: ${setBGImage};
+    width: ${setSizeImage};
+    margin-top: ${setPositionTop}%;
+    left: ${setPositionLeft}%;
   }
 `;
 
-export const Photo = styled.div`
+export const Filter = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  backdrop-filter: grayscale(80%);
+  transition: backdrop-filter 0.4s ease-in-out;
+  &:hover {
+    backdrop-filter: grayscale(0%);
+    // for Safari always use -webkit- prefix
+    --webkit-backdrop-filter: grayscale(0%);
+  }
+  &:before {
+    content: '';
+    display: block;
+    padding-top: 100%; /* initial ratio of 1:1*/
+  }
+`;
+export const ContentContainer = styled.div`
+  overflow: hidden;
+  position: relative;
+  padding-bottom: 20px;
+  width: 100%;
+
   ${mediaBp('tablet')} {
+    padding-bottom: 0;
+    width: 50%;
   }
   ${mediaBp('desktop')} {
-    width: 100%;
-    height: 100%;
-    backdrop-filter: grayscale(80%);
-    transition: backdrop-filter 0.4s ease-in-out;
-    &:hover {
-      backdrop-filter: grayscale(0%);
-      // for Safari always use -webkit- prefix
-      --webkit-backdrop-filter: grayscale(0%);
-    }
+    padding-bottom: 0;
+    width: 50%;
   }
 `;
-
 export const TextBlock = styled.p`
+  font-family: Poppins;
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  bottom: 0;
+  right: 0;
+  font-size: calc(130px + (310 - 130) * ((100vw - 320px) / (770 - 320)));
+  line-height: calc(104px + (250 - 104) * ((100vw - 320px) / (770 - 320)));
+  font-weight: 900;
+  text-transform: uppercase;
+  opacity: 0.3;
+  background: var(--2-Shadov, linear-gradient(270deg, #292929 2.75%, rgba(143, 143, 143, 0) 174.73%));
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  mix-blend-mode: color-burn;
+  filter: blur(2.5px);
+  word-break: break-word;
   ${mediaBp('tablet')} {
+    top: 1%;
+    left: -4%;
+    line-height: calc(94px + (320 - 94) * ((100vw - 600px) / (1920 - 600)));
+    font-size: calc(136px + (400 - 136) * ((100vw - 600px) / (1920 - 600)));
   }
   ${mediaBp('desktop')} {
-    height: 960px;
-    width: 920px;
-    text-align: left;
-    font-size: 400px;
-    font-weight: 900;
-    line-height: 80%;
-    text-transform: uppercase;
-    opacity: 0.3;
-    background: var(--2-Shadov, linear-gradient(270deg, #292929 2.75%, rgba(143, 143, 143, 0) 174.73%));
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    mix-blend-mode: color-burn;
-    filter: blur(2.5px);
-
-    word-break: break-word;
+    top: -2%;
+    left: -4%;
+    line-height: calc(104px + (320 - 104) * ((100vw - 600px) / (1920 - 600)));
+    font-size: calc(150px + (410 - 150) * ((100vw - 600px) / (1920 - 600)));
+  }
+`;
+export const TextBlockRevers = styled.p`
+  font-family: Poppins;
+  position: absolute;
+  top: 0%;
+  left: 0%;
+  bottom: 0;
+  right: 0;
+  font-size: calc(130px + (310 - 130) * ((100vw - 320px) / (770 - 320)));
+  line-height: calc(104px + (250 - 104) * ((100vw - 320px) / (770 - 320)));
+  font-weight: 900;
+  text-transform: uppercase;
+  opacity: 0.3;
+  background: var(--2-Shadov, linear-gradient(270deg, #292929 2.75%, rgba(143, 143, 143, 0) 174.73%));
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  mix-blend-mode: color-burn;
+  filter: blur(2.5px);
+  word-break: break-word;
+  ${mediaBp('tablet')} {
+    text-align: end;
+    top: 1%;
+    left: -4%;
+    line-height: calc(94px + (320 - 94) * ((100vw - 600px) / (1920 - 600)));
+    font-size: calc(136px + (400 - 136) * ((100vw - 600px) / (1920 - 600)));
+  }
+  ${mediaBp('desktop')} {
+    text-align: end;
+    top: -2%;
+    left: -4%;
+    line-height: calc(104px + (320 - 104) * ((100vw - 600px) / (1920 - 600)));
+    font-size: calc(150px + (410 - 150) * ((100vw - 600px) / (1920 - 600)));
   }
 `;
 
@@ -132,11 +217,7 @@ export const ColoredText = styled.span`
 `;
 
 export const GreyText = styled.span`
-  ${mediaBp('tablet')} {
-  }
-  ${mediaBp('desktop')} {
-    color: #b2b2b2;
-  }
+  color: #b2b2b2;
 `;
 
 export const TextWrapper = styled.div`
@@ -171,19 +252,26 @@ export const Content = styled(motion.p)`
   color: #292929;
   text-align: justify;
   font-family: Poppins;
-  font-size: 14px;
   font-style: normal;
   font-weight: 400;
-  line-height: 130%; /* 18.2px */
-  width: 230px;
-  text-indent: 62px;
+  line-height: 100%;
+  font-size: calc(10px + (16 - 10) * ((100vw - 320px) / (768 - 320)));
+  width: 47%;
+  text-indent: 35px;
+  margin-bottom: calc(10px + (70 - 20) * ((100vw - 320px) / (1920 - 320)));
   ${mediaBp('tablet')} {
+    line-height: 130%; /* 18.2px */
+    text-indent: 40px;
+    font-size: calc(10px + (16 - 10) * ((100vw - 768px) / (1280 - 768)));
+    width: calc(178px + (376 - 178) * ((100vw - 768px) / (1920 - 768)));
+    margin-bottom: calc(12px + (30 - 12) * ((100vw - 768px) / (1280 - 768)));
   }
   ${mediaBp('desktop')} {
+    line-height: 130%; /* 18.2px */
     text-indent: 45px;
-    font-size: 18px;
-    width: 376px;
-    margin-bottom: 98px;
+    font-size: calc(16px + (18 - 16) * ((100vw - 1280px) / (1920 - 1280)));
+    width: calc(178px + (376 - 178) * ((100vw - 768px) / (1920 - 768)));
+    margin-bottom: calc(20px + (80 - 20) * ((100vw - 1280px) / (1920 - 1280)));
   }
 `;
 export const BlurContent = styled.span`
@@ -193,64 +281,57 @@ export const MainBtn = styled.button`
   display: flex;
   flex-direction: column;
   border: none;
+  background-color: transparent;
+`;
+
+export const BtnBlock = styled.div`
+  position: absolute;
+  text-align: center;
+  left: 1%;
+  bottom: 0%;
+  ${mediaBp('tablet')} {
+    position: absolute;
+    text-align: center;
+    left: 2%;
+    bottom: 0%;
+  }
+  ${mediaBp('desktop')} {
+    position: absolute;
+    text-align: center;
+    left: 2%;
+    bottom: 0;
+  }
+`;
+export const BtnText = styled(motion.p)`
   color: #e50185;
   font-family: 'PhontPhreaksHandwriting';
-  font-size: 40px;
+  font-size: calc(30px + (64 - 30) * ((100vw - 768px) / (1920 - 768)));
   font-style: normal;
   font-weight: 400;
   line-height: 100%; /* 40px */
   letter-spacing: 2px;
   text-transform: uppercase;
-  background-color: transparent;
-  ${mediaBp('tablet')} {
-  }
-  ${mediaBp('desktop')} {
-    font-size: 64px;
-    letter-spacing: 3.2px;
-  }
 `;
-export const HoverBox = styled.div``;
-export const PositionBottom = styled.div`
-  left: 75%;
-  bottom: 0;
+export const BtnBlockRevers = styled.div`
   position: absolute;
-`;
-export const PositionTop240 = styled.div`
-  top: 240px;
-  left: 62%;
-  position: absolute;
-`;
-export const BtnBlock = styled.div`
+  text-align: center;
+  left: 51%;
+  bottom: 0%;
+  width: 100%;
   ${mediaBp('tablet')} {
-  }
-  ${mediaBp('desktop')} {
-    padding-left: 40px;
     position: absolute;
     text-align: center;
     left: 50%;
     bottom: 0;
   }
-`;
-export const BtnBlockRevers = styled.div`
-  ${mediaBp('tablet')} {
-  }
   ${mediaBp('desktop')} {
     position: absolute;
     text-align: center;
-    left: 26%;
+    left: 52%;
     bottom: 0;
   }
 `;
-export const PositionTop0 = styled.div`
-  top: 0px;
-  left: 25%;
-  position: absolute;
-`;
-export const PositionRight0 = styled.div`
-  top: 240px;
-  left: 0;
-  position: absolute;
-`;
+
 export const RelativBox = styled.div`
   position: relative;
   width: 100%;
@@ -278,4 +359,13 @@ export const LineBox = styled(motion.svg)`
     top: -210px;
     left: 38%;
   }
+`;
+
+export const BackDrop = styled(motion.div)`
+  top: 0%;
+  position: absolute;
+  background: linear-gradient(180deg, rgb(239, 239, 239, 1) 74%, rgb(239, 239, 239, 0.01) 100%);
+  width: 100%;
+  height: 110%;
+  z-index: 5;
 `;
